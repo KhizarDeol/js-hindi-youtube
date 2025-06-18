1758e1a8a795dce880926d15d541aa2637115c75
KhizarDeol <mkhizar4141@gmail.com>
2025-06-18 15:51:11 +0000

    update

--- 01-basics/04-operations.js
+++ 01-basics/04-operations.js
@@ -1,3 +1,28 @@
 let value = 3
-let negValve = -value 
-console.log(negValue)
+let negValue = -value 
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**7);
+console.log(2/6);
+console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " Khizar"
+let str3  = str1 + str2
+console.log(str3);
+
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter) 
